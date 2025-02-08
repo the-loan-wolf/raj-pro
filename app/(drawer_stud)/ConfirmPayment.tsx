66a195a07dd23ef3e92f021ea8ae57d7bac55b5f
rajkumar <rajkumar2237701@gmail.com>
2025-02-08 11:34:19 +0530

    added new route ConfirmPayment

---
+++ app/(drawer_stud)/ConfirmPayment.tsx
@@ -0,0 +1,65 @@
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
+import paymentConfirm from "@/utils/paymentConfirm";
+import { PaymentData } from "@/utils/type";
+
+const ConfirmPayment = ({ paymentData }: { paymentData: PaymentData }) => {
+  const { payment_id } = paymentData;
+  return (
+    // If the shop has generated the offer, show this view.
+    <View style={styles.card}>
+      <Text style={styles.title}>Payment Details</Text>
+      <Text style={styles.text}>Payment ID: {paymentData.payment_id}</Text>
+      <Text style={styles.text}>Offer ID: {paymentData.offer_id}</Text>
+      <Text style={styles.text}>Offer Amount: {paymentData.offer_amount}</Text>
+      <Text style={styles.text}>
+        Remaining Amount: {paymentData.remaining_amount_to_pay}
+      </Text>
+      <Text style={styles.text}>Message: {paymentData.message}</Text>
+      <TouchableOpacity
+        style={styles.verifyButton}
+        onPress={() => paymentConfirm(payment_id)}
+      >
+        <Text style={styles.text}>Confirm</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default ConfirmPayment;
+
+const styles = StyleSheet.create({
+  card: {
+    width: "90%",
+    padding: 30,
+    borderRadius: 15,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "700",
+    color: "#0A3981",
+    marginBottom: 20,
+  },
+  text: {
+    fontSize: 16,
+    color: "#0A3981",
+    marginBottom: 10,
+  },
+  verifyButton: {
+    backgroundColor: "red",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "80%",
+    position: "absolute",
+    bottom: 50,
+  },
+  buttonText: {
+    fontSize: 16,
+    color: "white",
+    fontWeight: "700",
+  },
+});
--- app/(drawer_stud)/watingForTransaction.tsx
+++ app/(drawer_stud)/watingForTransaction.tsx
@@ -5,7 +5,7 @@ import getToken from "@/utils/getToken";
 import { useEffect, useState } from "react";
 import fetchOfferDetails from "@/utils/fetchOfferDetails";
 import { PaymentData } from "@/utils/type";
-import paymentConfirm from "@/utils/paymentConfirm";
+import ConfirmPayment from "./ConfirmPayment";
 
 type Props = {
   id: number;
@@ -63,20 +63,7 @@ const WatingForTransaction = ({ id }: Props) => {
     }
   };
   return paymentData ? (
-    // If the shop has generated the offer, show this view.
-    <View style={styles.card}>
-      <Text style={styles.title}>Payment Details</Text>
-      <Text style={styles.text}>Payment ID: {paymentData.payment_id}</Text>
-      <Text style={styles.text}>Offer ID: {paymentData.offer_id}</Text>
-      <Text style={styles.text}>Offer Amount: {paymentData.offer_amount}</Text>
-      <Text style={styles.text}>
-        Remaining Amount: {paymentData.remaining_amount_to_pay}
-      </Text>
-      <Text style={styles.text}>Message: {paymentData.message}</Text>
-      <TouchableOpacity style={styles.verifyButton} onPress={() => paymentConfirm(paymentId)}>
-        <Text style={styles.text}>Confirm</Text>
-      </TouchableOpacity>
-    </View>
+    <ConfirmPayment paymentData={paymentData} />
   ) : (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <View style={{ marginBlockEnd: 50 }}>
@@ -94,23 +81,6 @@ const WatingForTransaction = ({ id }: Props) => {
 export default WatingForTransaction;
 
 const styles = StyleSheet.create({
-  card: {
-    width: "90%",
-    padding: 30,
-    borderRadius: 15,
-    alignItems: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "700",
-    color: "#0A3981",
-    marginBottom: 20,
-  },
-  text: {
-    fontSize: 16,
-    color: "#0A3981",
-    marginBottom: 10,
-  },
   verifyButton: {
     backgroundColor: "red",
     paddingVertical: 10,
